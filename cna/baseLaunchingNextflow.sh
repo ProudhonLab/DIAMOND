abfb336e25d5ce4ffaef87657a8a559620c31485
K.V.g <38720203+KlausVG@users.noreply.github.com>
2024-07-02 14:19:16 +0200

    Update baseLaunchingNextflow.sh

--- cna/baseLaunchingNextflow.sh
+++ cna/baseLaunchingNextflow.sh
@@ -8,4 +8,3 @@ conda activate /scratch/kvongrafenst/Nextflow
 
 echo $mail
 nextflow run  $2 $3 $4 $5 $6 $7 $8 $9 -N ${1} -with-timeline -with-report -resume
-#sbatch baseLaunchingNextflow.sh klaus.von-grafenstein@univ-rennes.fr workflowPCRUnique.nf
